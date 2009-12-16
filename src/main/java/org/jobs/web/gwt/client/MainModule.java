d635c47e3b43dc0ebcd00cde59df1633684d6cd8
unknown <vit@.(none)>
2009-12-16 02:04:32 +0200

    login sample

--- src/main/java/org/jobs/web/gwt/client/MainModule.java
+++ src/main/java/org/jobs/web/gwt/client/MainModule.java
@@ -1,21 +1,47 @@
 package org.jobs.web.gwt.client;
 
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.Info;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.layout.CenterLayout;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class MainModule implements EntryPoint {
 
 	public void onModuleLoad() {
 
-		final Button button = new Button("Say Hello", new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				Info.display("Title", "Message");
+		final FormPanel simple = new FormPanel();
+		simple.setHeading("Login Form");
+		simple.setFrame(true);
+
+		simple.setWidth(350);
+
+		final TextField<String> login = new TextField<String>();
+		login.setFieldLabel("Login");
+		simple.add(login);
+
+		final TextField<String> password = new TextField<String>();
+		password.setFieldLabel("Password");
+		password.setPassword(true);
+		simple.add(password);
+
+		final Button button = new Button("Login");
+		button.addSelectionListener(new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				if (!simple.isValid()) {
+					return;
+				}
+				Info.display("Login", "Hello " + login.getValue() + "!");
 			}
 		});
-		RootPanel.get().add(button);
+		simple.add(button);
+
+		RootPanel.get().add(simple);
+		simple.setLayout(new CenterLayout());
 	}
 }
