4b165f82a9a04f3e443e58795fd314519b5835e1
vitali.rydvan <vitali.rydvan@rydvan-v.grodno.iforum.com>
2009-12-17 10:18:57 +0200

    u

---
+++ conf/install.sh
@@ -0,0 +1,5 @@
+#mvn install:install-file -DgroupId=org.gwt -DartifactId=mosaic -Dversion=0.3.0-rc1 -Dpackaging=jar -Dfile=gwt-mosaic-0.3.0-rc1.jar
+#mvn install:install-file -DgroupId=com.allen_sauer -DartifactId=gwt -Dversion=2.6.5 -Dpackaging=jar -Dfile=gwt-dnd-2.6.5.jar
+#mvn install:install-file -DgroupId=com.google.gwt -DartifactId=libideas -Dversion=r1684 -Dpackaging=jar -Dfile=gwt-incubator-trunk-r1684.jar
+#mvn install:install-file -DgroupId=org.gwt -DartifactId=beansbinding -Dversion=0.2.4 -Dpackaging=jar -Dfile=gwt-beans-binding-0.2.4.jar
+mvn install:install-file -DgroupId=com.extjs -DartifactId=gxt -Dversion=2.1.0 -Dpackaging=jar -Dfile=gxt.jar
---
+++ run.sh
@@ -0,0 +1,1 @@
+mvn clean compile war:exploded gwt:run
